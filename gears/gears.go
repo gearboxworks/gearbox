3c5e932bf87e793a87e98828a632dc895365cad4
Saru Tole <sarutole@gmail.com>
2019-04-18 12:36:41 +0300

    Use filepath.FromSlash to have portable directory separator

--- gears/gears.go
+++ gears/gears.go
@@ -16,6 +16,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 type Gear interface {
@@ -98,13 +99,13 @@ func (me *Gears) Initialize() (sts status.Status) {
 		b, sc, sts = util.HttpRequest(JsonUrl)
 		if status.IsError(sts) || sc != http.StatusOK { // @TODO Bundle these as Assets so we will always have some options
 			log.Print("Could not download 'gears.json' and no options have previously been stored.")
-			filepath := fmt.Sprintf("%s/%s", me.OsSupport.GetAdminRootDir(), JsonFilename)
+			fp := filepath.FromSlash(fmt.Sprintf("%s/%s", me.OsSupport.GetAdminRootDir(), JsonFilename))
 			var err error
-			log.Printf("Loading included '%s'.", filepath)
-			b, err = ioutil.ReadFile(filepath)
+			log.Printf("Loading included '%s'.", fp)
+			b, err = ioutil.ReadFile(fp)
 			if err != nil {
 				sts = status.Fail(&status.Args{
-					Message: fmt.Sprintf("unable to read '%s'", filepath),
+					Message: fmt.Sprintf("unable to read '%s'", fp),
 				})
 				break
 			}
