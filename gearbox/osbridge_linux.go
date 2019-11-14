2590a988b0b788ca67b787ed68c928b591381bea
pr0logas <prologas@protonmail.com>
2019-11-14 19:27:16 +0200

    Compilation for Windows/Linux

--- gearbox/osbridge_linux.go
+++ gearbox/osbridge_linux.go
@@ -4,10 +4,11 @@ package gearbox
 
 import (
 	"gearbox/global"
+	"gearbox/types"
 	"github.com/gearboxworks/go-osbridge"
 )
 
-func GetOsBridge(project types.Name, userdata types.RelativePath) *osbridge.OsBridge {
+func GetOsBridge(project types.Name, userdata types.Path) *osbridge.OsBridge {
 	return osbridge.NewOsBridge(&osbridge.Args{
 		ProjectName:  project,
 		UserDataPath: userdata,
